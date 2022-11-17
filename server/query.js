41d4964dc6309e3f0fa70320bee0f8a7c88d0c3d
Adminstrator <adminstrator@v1040-wn-rt-b-2-118.campus-dynamic.uwaterloo.ca>
2022-11-17 17:30:54 -0500

    delete cascade + unique (course_id, name) in assignment

--- server/query.js
+++ server/query.js
@@ -5,12 +5,12 @@ create_queries = [ // see create_tables.sql
     "CREATE TYPE usertype AS ENUM ('student', 'staff', 'admin')",
     'CREATE TABLE "User" (id SERIAL PRIMARY KEY, name TEXT NOT NULL, email TEXT NOT NULL UNIQUE, password_hash TEXT NOT NULL, type USERTYPE)',
     "CREATE TABLE Course (id SERIAL PRIMARY KEY, name TEXT NOT NULL)",
-    'CREATE TABLE Teaches (course_id INT, staff_id INT, FOREIGN KEY (course_id) REFERENCES Course(id), FOREIGN KEY (staff_id) REFERENCES "User"(id))',
-    'CREATE TABLE EnrolledIn (course_id INT, student_id INT, FOREIGN KEY (course_id) REFERENCES Course(id), FOREIGN KEY (student_id) REFERENCES "User"(id))',
-    'CREATE TABLE Assignment (id SERIAL PRIMARY KEY, course_id INT, name TEXT NOT NULL, deadline TIMESTAMP, max_grade INT CHECK (max_grade >= 0), description TEXT DEFAULT \'\', FOREIGN KEY (course_id) REFERENCES Course(id))',
-    'CREATE TABLE Question (assignment_id INT NOT NULL, number INT, max_grade INT CHECK (max_grade >= 0), description TEXT, FOREIGN KEY (assignment_id) REFERENCES Assignment(id), PRIMARY KEY (assignment_id, number))',
-    'CREATE TABLE QuestionSubmission (student_id INT, assignment_id INT NOT NULL, question_number INT, file_path TEXT DEFAULT \'\', grade INT CHECK (grade >= 0), staff_comments TEXT DEFAULT \'\', FOREIGN KEY (student_id) REFERENCES "User"(id), FOREIGN KEY (assignment_id, question_number) REFERENCES Question(assignment_id, number), PRIMARY KEY (student_id, assignment_id, question_number))',
-    'CREATE TABLE AssignmentSubmission (student_id INT, assignment_id INT NOT NULL, grade INT, is_submitted BOOLEAN, FOREIGN KEY (student_id) REFERENCES "User"(id), FOREIGN KEY (assignment_id) REFERENCES Assignment(id))'
+    'CREATE TABLE Teaches (course_id INT, staff_id INT, FOREIGN KEY (course_id) REFERENCES Course(id) ON DELETE CASCADE, FOREIGN KEY (staff_id) REFERENCES "User"(id) ON DELETE CASCADE)',
+    'CREATE TABLE EnrolledIn (course_id INT, student_id INT, FOREIGN KEY (course_id) REFERENCES Course(id) ON DELETE CASCADE, FOREIGN KEY (student_id) REFERENCES "User"(id) ON DELETE CASCADE)',
+    'CREATE TABLE Assignment (id SERIAL PRIMARY KEY, course_id INT, name TEXT NOT NULL, deadline TIMESTAMP, max_grade INT CHECK (max_grade >= 0), description TEXT DEFAULT \'\', UNIQUE (course_id, name), FOREIGN KEY (course_id) REFERENCES Course(id) ON DELETE CASCADE)',
+    'CREATE TABLE Question (assignment_id INT NOT NULL, number INT, max_grade INT CHECK (max_grade >= 0), description TEXT, FOREIGN KEY (assignment_id) REFERENCES Assignment(id) ON DELETE CASCADE, PRIMARY KEY (assignment_id, number))',
+    'CREATE TABLE QuestionSubmission (student_id INT, assignment_id INT NOT NULL, question_number INT, file_path TEXT DEFAULT \'\', grade INT CHECK (grade >= 0), staff_comments TEXT DEFAULT \'\', FOREIGN KEY (student_id) REFERENCES "User"(id) ON DELETE CASCADE, FOREIGN KEY (assignment_id, question_number) REFERENCES Question(assignment_id, number) ON DELETE CASCADE, PRIMARY KEY (student_id, assignment_id, question_number))',
+    'CREATE TABLE AssignmentSubmission (student_id INT, assignment_id INT NOT NULL, grade INT, is_submitted BOOLEAN, FOREIGN KEY (student_id) REFERENCES "User"(id) ON DELETE CASCADE, FOREIGN KEY (assignment_id) REFERENCES Assignment(id) ON DELETE CASCADE)'
 ]
 
 drop_queries = [
@@ -100,26 +100,11 @@ const Query = {
     }, 
 
     async deleteCourse(req, res, cid) {
-        await multiQuery(req, res, 
-            [`DELETE FROM teaches WHERE course_id=${cid}`,
-            `DELETE FROM enrolledin WHERE course_id=${cid}`,
-            `DELETE FROM questionsubmission WHERE course_id=${cid}`,
-            `DELETE FROM question WHERE course_id=${cid}`,
-            `DELETE FROM assignmentsubmission WHERE course_id=${cid}`,
-            `DELETE FROM assignment WHERE course_id=${cid}`,
-            `DELETE FROM course WHERE id=${cid}`
-            ]
-        )
+        await query(req, res,`DELETE FROM course WHERE id=${cid}`)
     }, 
 
     async deleteAssignment(req, res, aid) {
-        await multiQuery(req, res, 
-            [`DELETE FROM questionsubmission WHERE assignment_id=${aid}`,
-            `DELETE FROM question WHERE assignment_id=${aid}`,
-            `DELETE FROM assignmentsubmission WHERE assignment_id=${aid}`,
-            `DELETE FROM assignment WHERE id=${aid}`
-            ]
-        )
+        await query(req, res, `DELETE FROM assignment WHERE id=${aid}`)
     }, 
     
     async run(req, res, q) { // gary dw this is very secure, no ACE here
