bf71cd61eafa7fe9b45eef285d322635fc798cfd
Adminstrator <adminstrator@Admins-MacBook-Pro.local>
2022-12-01 15:29:21 -0500

    add assignmentsubmissions for new student

--- server/server.js
+++ server/server.js
@@ -47,6 +47,10 @@ app.get("/user", (req, res) => {
     Query.getUser(req, res, req.query.id);
 })
 
+app.get("/allUsers", (req, res) => {
+    Query.getAllUsers(req, res);
+})
+
 app.get("/usersInCourse", (req, res) => {
     Query.getUsersInCourse(req, res, req.query.cid);
 })
@@ -61,6 +65,10 @@ app.get("/courses", (req, res) => {
 
 app.get("/coursestudents", (req, res) => {
     Query.getCourseStudents(req, res, req.query.cid);
+})
+
+app.get("/enrollStudent", (req, res) => {
+    Query.enrollStudent(req, res, req.query.cid, req.query.uid);
 })
 
 app.get("/assignment", (req, res) => {
